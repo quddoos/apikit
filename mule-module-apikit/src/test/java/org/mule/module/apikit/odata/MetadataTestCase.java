0be7fba919f6cf647e513d7acc33b1c8b3073599
mcoaker <mhcoaker@gmail.com>
2015-08-04 17:52:21 -0300

    adding odata tests

---
+++ mule-module-apikit/src/test/java/org/mule/module/apikit/odata/MetadataTestCase.java
@@ -0,0 +1,86 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.module.apikit.odata;
+
+import static com.jayway.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.junit.matchers.JUnitMatchers.hasItems;
+
+import org.mule.tck.junit4.FunctionalTestCase;
+import org.mule.tck.junit4.rule.DynamicPort;
+
+import com.jayway.restassured.RestAssured;
+
+import org.junit.Rule;
+import org.junit.Test;
+
+public class MetadataTestCase extends FunctionalTestCase
+{
+
+    @Rule
+    public DynamicPort serverPort = new DynamicPort("serverPort");
+
+    @Override
+    public int getTestTimeoutSecs()
+    {
+        return 6000;
+    }
+
+    @Override
+    protected void doSetUp() throws Exception
+    {
+        RestAssured.port = serverPort.getNumber();
+        super.doSetUp();
+    }
+
+    @Override
+    protected String getConfigResources()
+    {
+        return "org/mule/module/apikit/resource/root-resource-config.xml";
+    }
+
+    @Test
+    public void serviceSheetJsonPositive() throws Exception
+    {
+        given().header("Accept", "application/json")
+            .expect()
+                .response().body("value.name", hasItems("Products", "PersonDetails"))
+                .header("Content-type", "application/json").statusCode(200)
+            .when().get("/odata.svc?$format=json");
+    }
+    
+    @Test
+    public void serviceSheetXmlPositive() throws Exception
+    {
+        given().header("Accept", "application/xml")
+            .expect()
+                .response().body("service.workspace.collection", hasItems("ProductDetails"))
+                .header("Content-type", "application/xml").statusCode(200)
+            .when().get("/odata.svc");
+    }
+    
+    @Test
+    public void MetadataPositive() throws Exception
+    {
+        given().header("Accept", "application/xml")
+            .expect()
+                .response().body("DataServices.Schema.EntityType", hasItems("Product"))
+                .header("Content-type", "application/xml").statusCode(200)
+            .when().get("/odata.svc?$metadata");
+    }
+    
+    @Test
+    public void getUsersPositive() throws Exception
+    {
+        given().header("Accept", "application/json")
+            .expect()
+                .response().body("value.Name", hasItems("Paula Wilson", "Jose Pavarotti"))
+                .header("Content-type", "application/json").statusCode(200)
+            .when().get("/odata.svc/users?$format=json");
+    }
+
+}
---
+++ mule-module-apikit/src/test/java/org/mule/module/apikit/odata/ServiceDocumentTestCase.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.module.apikit.odata;
+
+import static com.jayway.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.junit.matchers.JUnitMatchers.hasItems;
+
+import org.mule.tck.junit4.FunctionalTestCase;
+import org.mule.tck.junit4.rule.DynamicPort;
+
+import com.jayway.restassured.RestAssured;
+
+import org.junit.Rule;
+import org.junit.Test;
+
+public class ServiceDocumentTestCase extends FunctionalTestCase
+{
+
+    @Rule
+    public DynamicPort serverPort = new DynamicPort("serverPort");
+
+    @Override
+    public int getTestTimeoutSecs()
+    {
+        return 6000;
+    }
+
+    @Override
+    protected void doSetUp() throws Exception
+    {
+        RestAssured.port = serverPort.getNumber();
+        super.doSetUp();
+    }
+
+    @Override
+    protected String getConfigResources()
+    {
+        return "org/mule/module/apikit/resource/odata/odata.xml";
+    }
+
+    
+    @Test
+    public void serviceSheetJsonPositive() throws Exception
+    {
+        given().header("Accept", "application/json")
+            .expect()
+                .response().body("value.name", hasItems("orders", "customers"))
+                .header("Content-type", "application/json").statusCode(200)
+            .when().get("/odata.svc?$format=json");
+    }
+    
+    @Test
+    public void serviceSheetXmlPositive() throws Exception
+    {
+        given().header("Accept", "application/xml")
+            .expect()
+                .response().body("service.workspace.collection", hasItems("orders"))
+                .header("Content-type", "application/xml").statusCode(200)
+            .when().get("/odata.svc");
+    }
+
+}
