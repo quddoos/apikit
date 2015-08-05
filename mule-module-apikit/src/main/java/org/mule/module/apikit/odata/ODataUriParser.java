1213f1d5315be3f4a1b596bbaa0f7d410fe9c3f8
mcoaker <mhcoaker@gmail.com>
2015-08-05 17:31:15 -0300

    fixing yet more odata related tests

--- mule-module-apikit/src/main/java/org/mule/module/apikit/odata/ODataUriParser.java
+++ mule-module-apikit/src/main/java/org/mule/module/apikit/odata/ODataUriParser.java
@@ -19,92 +19,100 @@ import org.mule.module.apikit.odata.processor.ODataServiceDocumentProcessor;
 
 public class ODataUriParser {
 
-    private static final String ODATA_SVC_URI_PREFIX = "/odata.svc";
-    private static final String[] ODATA_VALID_PARAMS = {"orderby", "top", "skip", "filter", "expand", "format", "select", "inlinecount"};
+	private static final String ODATA_SVC_URI_PREFIX = "/odata.svc";
+	private static final String[] ODATA_VALID_PARAMS = { "$orderby", "$top",
+			"$skip", "$filter", "$expand", "$format", "$select", "$inlinecount" };
 
-    /**
-     * Parses the URI and returns the right processor to handle the request
-     * 
-     * MAKE IT NICE AND SHINY! TEST IT!
-     * 
-     * @param event
-     * @return
-     * @throws ODataInvalidQueryRequestException
-     */
-    public static ODataRequestProcessor parse(String path, String query)
-	    throws ODataInvalidQueryRequestException {
+	/**
+	 * Parses the URI and returns the right processor to handle the request
+	 * 
+	 * MAKE IT NICE AND SHINY! TEST IT!
+	 * 
+	 * @param event
+	 * @return
+	 * @throws ODataInvalidQueryRequestException
+	 */
+	public static ODataRequestProcessor parse(String path, String query)
+			throws ODataInvalidQueryRequestException {
 
-	path = path.replace(ODATA_SVC_URI_PREFIX, "");
-	try { 
-	    query = URLDecoder.decode(query, "UTF-8");
-	} catch (Exception e) {
-	    throw new ODataInvalidQueryRequestException(e.getMessage());
-	}
-
-	// metadata
-	if (path.contains("/$metadata")) {
-	    if (path.equals("/$metadata") && query.isEmpty()) {
-		return new ODataMetadataProcessor();
-	    }
-	    throw new ODataInvalidQueryRequestException();
-	}
-
-	// service document
-	if (path.isEmpty() || path.equals("/")) {
-	    if (query.isEmpty() || query.startsWith("$format=")) {
-		return new ODataServiceDocumentProcessor();
-	    }
-	    throw new ODataInvalidQueryRequestException();
-	}
-
-	// entity(s) request
-	if (path.matches("^/[a-zA-Z0-9]+\\(*[a-zA-Z0-9]*\\)*$")) {
-	    String pattern = "^/([a-zA-Z0-9]+)\\(*([a-zA-Z0-9])\\)*$";
-	    Pattern r = Pattern.compile(pattern);
-	    Matcher m = r.matcher(path);
-
-	    String id = "";
-	    String querystring = "";
-
-	    // handle id
-	    if (m.groupCount() == 2) {
-		id = "/" + m.group(1);
-	    }
-
-	    // handle query
-	    if (!query.isEmpty()) {
-		String[] queryParams = query.replace("?","").split("&");
-		for (String queryParam : queryParams) {
-		    String[] elems = queryParam.split("=");
-		    if (elems.length == 2 || !validQueryParam(elems[0])) {
-			throw new ODataInvalidQueryRequestException("The query parameter '$" + elems[0]  + "' begins with a system-reserved '$' character but is not recognized.");
-		    }
+		path = path.replace(ODATA_SVC_URI_PREFIX, "");
+		try {
+			query = URLDecoder.decode(query, "UTF-8");
+		} catch (Exception e) {
+			throw new ODataInvalidQueryRequestException(e.getMessage());
 		}
-		querystring = query.replace("$", "");
-	    }
-	    	    
-	    // build path
-	    String apikitFriendlyUri = m.group(1) + id + querystring;
 
-	    return new ODataApikitProcessor(apikitFriendlyUri);
-	} else {
-	    String pattern = "^/([a-zA-Z0-9]+)/";
-	    Pattern r = Pattern.compile(pattern);
-	    Matcher m = r.matcher(path);
+		// metadata
+		if (path.contains("/$metadata")) {
+			if (path.equals("/$metadata") && query.isEmpty()) {
+				return new ODataMetadataProcessor();
+			}
+			throw new ODataInvalidQueryRequestException();
+		}
 
-	    String entity = "";
+		// service document
+		if (path.isEmpty() || path.equals("/")) {
+			if (query.isEmpty() || query.startsWith("$format=")) {
+				return new ODataServiceDocumentProcessor();
+			}
+			throw new ODataInvalidQueryRequestException();
+		}
 
-	    if (m.groupCount() == 1) {
-		entity = m.group(0);
-	    }
+		// entity(s) request
+		if (path.matches("^/[a-zA-Z0-9]+\\(*[a-zA-Z0-9]*\\)*$")) {
+			String pattern = "^/([a-zA-Z0-9]+)\\(*([a-zA-Z0-9]*)\\)*$";
+			Pattern r = Pattern.compile(pattern);
+			Matcher m = r.matcher(path);
 
-	    throw new ODataInvalidQueryRequestException("Resource not found for the segment '" + entity + "'.");
+			String id = "";
+			String querystring = "";
+
+			// handle id
+			if (m.matches() && !m.group(2).isEmpty()) {
+				id = "/" + m.group(2);
+			}
+
+			// handle query
+			querystring = handleQuerystring(query, querystring);
+
+			// build path
+			String apikitFriendlyUri = "/" + m.group(1) + id + querystring;
+
+			return new ODataApikitProcessor(apikitFriendlyUri);
+		} else {
+			String pattern = "^/([a-zA-Z0-9]+)/";
+			Pattern r = Pattern.compile(pattern);
+			Matcher m = r.matcher(path);
+
+			String entity = "";
+			entity = m.group(1);
+
+			throw new ODataInvalidQueryRequestException(
+					"Resource not found for the segment '" + entity + "'.");
+		}
+
 	}
 
-    }
-    
-    private static boolean validQueryParam(String param) {
-	return (Arrays.asList(ODATA_VALID_PARAMS).contains(param));
-    }
+	private static String handleQuerystring(String query, String querystring)
+			throws ODataInvalidQueryRequestException {
+		if (!query.isEmpty()) {
+			String[] queryParams = query.replace("?", "").split("&");
+			for (String queryParam : queryParams) {
+				String[] elems = queryParam.split("=");
+				if (elems.length == 2 || !validQueryParam(elems[0])) {
+					throw new ODataInvalidQueryRequestException(
+							"The query parameter '"
+									+ elems[0]
+									+ "' begins with a system-reserved '$' character but is not recognized.");
+				}
+			}
+			querystring = query.replace("$", "");
+		}
+		return querystring;
+	}
+
+	private static boolean validQueryParam(String param) {
+		return (Arrays.asList(ODATA_VALID_PARAMS).contains(param));
+	}
 
 }
