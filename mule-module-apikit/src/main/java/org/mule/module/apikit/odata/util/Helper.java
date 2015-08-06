4467e3100b12c1f0f02db782713c9ea88f968e8b
arielsegura <ariel.segura@yahoo.com>
2015-08-06 15:59:11 -0300

    Refresh metadata through URL

--- mule-module-apikit/src/main/java/org/mule/module/apikit/odata/util/Helper.java
+++ mule-module-apikit/src/main/java/org/mule/module/apikit/odata/util/Helper.java
@@ -1,6 +1,9 @@
 package org.mule.module.apikit.odata.util;
 
-import java.io.InputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
@@ -117,8 +120,10 @@ public class Helper {
 
 		return EdmSimpleType.STRING;
 	}
-	
-	public static GatewayMetadataManager refreshMetadataManager(Raml raml) throws GatewayMetadataMissingFieldsException, GatewayMetadataResourceNotFound, JSONException{
+
+	public static GatewayMetadataManager refreshMetadataManager(Raml raml)
+			throws GatewayMetadataMissingFieldsException,
+			GatewayMetadataResourceNotFound, JSONException {
 		gwMetadataManager.refreshMetadata(raml);
 		return gwMetadataManager;
 	}
@@ -143,12 +148,47 @@ public class Helper {
 	}
 
 	private static Raml getRaml(URL url) {
-		throw new NotImplementedException();
+		HttpURLConnection conn;
+		Raml raml = null;
+		try {
+			conn = (HttpURLConnection) url.openConnection();
+			conn.setRequestMethod("GET");
+			raml = new RamlDocumentBuilder().build(new InputStreamReader(
+					conn.getInputStream()));
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return raml;
 	}
 
 	private static Raml getRaml(String path) {
 		Raml raml = new RamlDocumentBuilder().build(path);
 		return raml;
+	}
+
+	public String getPayload(URL url) {
+		HttpURLConnection conn;
+		BufferedReader rd;
+		String line;
+		StringBuilder result = new StringBuilder();
+		try {
+			conn = (HttpURLConnection) url.openConnection();
+			conn.setRequestMethod("GET");
+			rd = new BufferedReader(
+					new InputStreamReader(conn.getInputStream()));
+			while ((line = rd.readLine()) != null) {
+				result.append(line);
+			}
+			rd.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return result.toString();
 	}
 
 }
---
+++ mule-module-apikit/src/test/java/org/mule/module/apikit/odata/HelperTest.java
@@ -0,0 +1,28 @@
+package org.mule.module.apikit.odata;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import junit.framework.Assert;
+
+import org.json.JSONException;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.mule.module.apikit.odata.metadata.exception.GatewayMetadataMissingFieldsException;
+import org.mule.module.apikit.odata.metadata.exception.GatewayMetadataNotInitializedException;
+import org.mule.module.apikit.odata.metadata.exception.GatewayMetadataResourceNotFound;
+import org.mule.module.apikit.odata.util.Helper;
+
+public class HelperTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Ignore
+	@Test
+	public void test() throws MalformedURLException, GatewayMetadataNotInitializedException, GatewayMetadataMissingFieldsException, GatewayMetadataResourceNotFound, JSONException {
+		Assert.assertEquals(Helper.refreshMetadataManager("datagateway-definition.raml").getEntitySet(), Helper.refreshMetadataManager(new URL("http://localhost:8081/api/config")).getEntitySet());
+	}
+}
